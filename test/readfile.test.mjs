663a0c3c8eb59de921b6c09e39d684c6a18a902c
王昱 <wangyu2021@foxmail.com>
2025-05-08 15:16:03 +0800

    move create and remove file to beforeEach and afterEach

--- test/readfile.test.mjs
+++ test/readfile.test.mjs
@@ -1,4 +1,4 @@
-import { describe, test } from "vitest";
+import { afterEach, beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
@@ -9,23 +9,23 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 describe("readFile tests", () => {
-  test("should read file content", async () => {
-    const testFilePath = join(__dirname, "test.rle");
-    const testContent = `#N Blinker
+  const testFilePath = join(__dirname, "test.rle");
+  const testContent = `#N Blinker
 #O John Conway
 #C A period 2 oscillator that is the smallest and most common oscillator.
 #C www.conwaylife.com/wiki/index.php?title=Blinker
 x = 3, y = 1, rule = B3/S23
 3o!`;
+  beforeEach(async () => {
+    await fs.writeFile(testFilePath, testContent);
+  });
 
-    try {
-      await fs.writeFile(testFilePath, testContent);
+  test("should read file content", async () => {
+    const result = await readFile(testFilePath);
+    expect(result).to.equal(testContent);
+  });
 
-      const result = await readFile(testFilePath);
-
-      expect(result).to.equal(testContent);
-    } finally {
-      await fs.unlink(testFilePath);
-    }
+  afterEach(async () => {
+    await fs.unlink(testFilePath);
   });
 });
