af26ae1f801e6ec2430fbbe49267dcfeb0316f07
王昱 <wangyu2021@foxmail.com>
2025-05-10 07:25:03 +0800

    implement getOutputPath

--- src/writefile.mjs
+++ src/writefile.mjs
@@ -1,9 +1,17 @@
 import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import { dirname, join, resolve } from "path";
 
 export const getInfoLine = (board) => {
   board.updateLength();
   board.updateHeight();
   return `x = ${board.length}, y = ${board.height}, rule = B3/S23`;
+};
+
+export const getOutputPath = () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = resolve(dirname(__filename), "../");
+  return join(__dirname, "output/output.rle");
 };
 
 export const writeFile = (filePath, str) => {
