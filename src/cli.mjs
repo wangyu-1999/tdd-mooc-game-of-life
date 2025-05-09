d359c90eb08178d77beb6ab3ef99a21eddab5544
王昱 <wangyu2021@foxmail.com>
2025-05-09 13:00:05 +0800

    implement getArgsFromCli and pass the test

---
+++ src/cli.mjs
@@ -0,0 +1,18 @@
+export const getArgsFromCli = () => {
+  const path = process.argv[2];
+  const iter = parseInt(process.argv[3]);
+  if (process.argv.length !== 4) {
+    throw new Error("the number of arguments is not correct");
+  }
+  const typeOfFile = path.split("/").pop().split(".").pop();
+  if (typeOfFile !== "rle") {
+    throw new Error("the file type is not correct");
+  }
+  if (isNaN(iter) || iter <= 0) {
+    throw new Error("the iter parameter is not correct");
+  }
+  return {
+    path,
+    iter,
+  };
+};
---
+++ test/cli.test.mjs
@@ -0,0 +1,37 @@
+import { afterEach, beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { getArgsFromCli } from "../src/cli.mjs";
+
+describe("CLI tests", () => {
+  const originalArgv = [...process.argv];
+
+  beforeEach(() => {
+    process.argv = originalArgv;
+  });
+
+  afterEach(() => {
+    process.argv = originalArgv;
+  });
+
+  test("getArgsFromCli should return an object with the correct properties", () => {
+    process.argv = ["node", "cli.mjs", "source/blinker.rle", "5"];
+    const { path, iter } = getArgsFromCli();
+    expect(path).to.equal("source/blinker.rle");
+    expect(iter).to.equal(5);
+  });
+
+  test("getArgsFromCli should throw an error if the number of arguments is incorrect", () => {
+    process.argv = ["node", "cli.mjs", "source/blinker.rle"];
+    expect(() => getArgsFromCli()).to.throw("the number of arguments is not correct");
+  });
+
+  test("getArgsFromCli should throw an error if the file type is not correct", () => {
+    process.argv = ["node", "cli.mjs", "source/blinker.txt", "5"];
+    expect(() => getArgsFromCli()).to.throw("the file type is not correct");
+  });
+
+  test("getArgsFromCli should throw an error if the iter parameter is not correct", () => {
+    process.argv = ["node", "cli.mjs", "source/blinker.rle", "-5"];
+    expect(() => getArgsFromCli()).to.throw("the iter parameter is not correct");
+  });
+});
