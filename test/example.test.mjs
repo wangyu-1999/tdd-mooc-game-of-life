b7540dcec65fc7fbc9689add7891192d69b21cae
王昱 <wangyu2021@foxmail.com>
2025-05-09 12:34:54 +0800

    delete example

--- test/example.test.mjs
+++
@@ -1,9 +0,0 @@
-import { describe, test } from "vitest";
-import { expect } from "chai";
-import { sum } from "../src/example.mjs";
-
-describe("Example test fixture", () => {
-  test("Example test", () => {
-    expect(sum(1, 2)).to.equal(3);
-  });
-});
