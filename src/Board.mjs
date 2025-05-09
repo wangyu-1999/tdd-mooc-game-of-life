59b62e3d1de11e1a5bd19e95c36c07177190f70c
王昱 <wangyu2021@foxmail.com>
2025-05-09 18:25:01 +0800

    implemet getBoardFromRLEString function

--- src/Board.mjs
+++ src/Board.mjs
@@ -49,6 +49,7 @@ export class Board {
     });
     return maxY - minY + 1;
   }
+
   static numberStrToNumber(str) {
     if (str === "") {
       return 1;
@@ -59,3 +60,41 @@ export class Board {
       throw new Error("Invalid string");
     }
   }
+
+  static getBoardFromRLEString(rleString) {
+    const board = new Board();
+    let x = 0;
+    let y = 0;
+    let numStr = "";
+    for (let i = 0; i < rleString.length; i++) {
+      if (rleString[i].match(/[0-9]/)) {
+        numStr += rleString[i];
+      } else if (rleString[i] === "b") {
+        // dead
+        const num = this.numberStrToNumber(numStr);
+        x += num;
+        numStr = "";
+      } else if (rleString[i] === "o") {
+        const num = this.numberStrToNumber(numStr);
+        for (let j = 0; j < num; j++) {
+          board.add(x, y);
+          x++;
+        }
+        numStr = "";
+      } else if (rleString[i] === "$") {
+        // end of the line
+        const num = this.numberStrToNumber(numStr);
+        y += num;
+        x = 0;
+        numStr = "";
+      } else if (rleString[i] === "!") {
+        // end of the string
+        break;
+      } else {
+        // unknown character
+        throw new Error("Invalid character");
+      }
+    }
+    return board;
+  }
+}
