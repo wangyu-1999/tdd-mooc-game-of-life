4a03f1eb871e9e690b9a92638fb4be44848c80a7
王昱 <wangyu2021@foxmail.com>
2025-05-10 07:13:55 +0800

    implement writeFile

--- src/writefile.mjs
+++ src/writefile.mjs
@@ -1,5 +1,11 @@
+import fs from "fs/promises";
+
 export const getInfoLine = (board) => {
   board.updateLength();
   board.updateHeight();
   return `x = ${board.length}, y = ${board.height}, rule = B3/S23`;
+};
+
+export const writeFile = (filePath, str) => {
+  fs.writeFile(filePath, str, "utf-8");
 };
